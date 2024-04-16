1ac9e36c5b423558da17494e6f1244efc3dc2e71
Topstail <chn740932867@163.com>
2024-04-16 22:58:08 +0800

    2404162258

--- string_test01.sh
+++ string_test01.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
+#
+#name="xuewenbuhao 944kaichebuhaozenmeban"
+## output:buhao 944kaiche
+##echo ${name:6}
+#
+## output: 944kaichebuhaozenmeban  # the output contain a empty
+#tmp1=${name#*buhao}
+#echo ${tmp1:0}
+## output:zenmeban
+#echo ${name##*buhao}
+## output:xuewenbuhao 944kaiche
+#echo ${name%buhao*}
+## output:xuewen
+#echo ${name%%buhao*}
+#
 
 name="xuewenbuhao 944kaichebuhaozenmeban"
-# output:buhao 944kaiche
-#echo ${name:6}
+echo $name
 
-# output: 944kaichebuhaozenmeban  # the output contain a empty
-tmp1=${name#*buhao}
-echo ${tmp1:0}
-# output:zenmeban
-echo ${name##*buhao}
-# output:xuewenbuhao 944kaiche
-echo ${name%buhao*}
-# output:xuewen
-echo ${name%%buhao*}
+# 
+# output:34
+echo $name | wc -L
+# output:35
+echo $name | wc -c
+# output:35
+echo $name | wc -m
+
+#
