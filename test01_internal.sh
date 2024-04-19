9c01bf122bd7a09b7b61170b72c7d64affcdc240
Topstail <chn740932867@163.com>
2024-04-19 15:20:46 +0800

    2404191520

--- test01_internal.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-var1="test01"
-
-# 使用source调用其他的shell脚本， 可以调用其他脚本声明的变量；这是和'bash 脚本名'命令的不同之处
-source test02_internal.sh
-
-echo ${var2}
--- test02_internal.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-echo ${var1}
-
-var2="test02"
